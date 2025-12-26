d2b57187a8eab4eb66c58755654b8209e0a24bdc
xa0627-sys <xa0627@gmail.com>
2025-12-26 21:39:03 +0800

    feat: add main App component

---
+++ app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>都市更新權利變換估價系統</h1>
+      </header>
+      <main>
+        <p>新应用镵項需為宫串賯羅之可矩形状</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
